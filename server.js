8e1c0ec376617766f24b10cc922b5cffbbba7b87
Leeland <leelandmiller@gmail.com>
2017-08-03 23:01:51 -0700

    schema.sql, add routes dir, update server.js

--- server.js
+++ server.js
@@ -6,11 +6,13 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
+var methodOverride = require('method-override');
 
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 8000;
 
 // Requiring our models for syncing
 var db = require("./models");
@@ -21,15 +23,21 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-// Static directory
-// app.use(express.static("public"));
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use('/public', express.static(__dirname + "/public"));
 
 // Routes
 // =============================================================
-// require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 // require("./routes/author-api-routes.js")(app);
 // require("./routes/post-api-routes.js")(app);
-
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
@@ -64,5 +72,3 @@ db.sequelize.sync({force:true}).then(function() {
 		})
 	});
 });
-
-
