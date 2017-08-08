f93667aabde37b5cbdc3174e4496c5231f84a98a
Leeland <leelandmiller@gmail.com>
2017-08-08 16:09:37 -0700

    added jumbotron, improved ui, implement signup page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,16 +3,25 @@ var path =  require("path");
 
 module.exports = function(app) {
 
+    // root route - runs Sequelize findAll() to show all profiles
     app.get('/', function(req, res) {
 
         db.profile.findAll({}).then(function(profiles){
-            res.render('index', { profiles, user: req.user });
+            res.render('index', { profiles, user: req.user, title: 'All Profiles', authentication: req.isAuthenticated() });
         });
 
     });
 
+    // linkedin-signup - renders sign-up page to register a new profile
     app.get('/linkedin-signup', function(req, res) {
-        res.render('sign-up');
+        res.render('sign-up', { user: req.user, title: 'Register Your Profile', authentication: req.isAuthenticated() });
+    });
+
+    // signup-submit - posts a new profile to db
+    app.post('/signup-submit', function(req, res) {
+        // console.log(res);
+        console.log(req.user);
+        console.log(req.body);
     });
 
     app.get('/signup', function(req,res){
