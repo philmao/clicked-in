9eea0bdb2b823d3eeba94d4d087daaac46f75ce7
philmao <philip_mao@yahoo.com>
2017-08-11 21:00:21 -0700

    add more test profiles

--- db/seeds.sql
+++ db/seeds.sql
@@ -101,3 +101,108 @@ VALUES (true, true, true, true, true, true, true, true,
 
 INSERT INTO projects (name, img_url, description, profileId)
 VALUES ('Company web site', '', 'Hi-Tech web site', 5);
+
+
+INSERT INTO profiles (name, img_url, title, about, linkedin_url, github_url, personal_url, endorsements, endorsed_people, linkedin_id, username, password)
+VALUES ('Shawn Fuller', 
+'https://randomuser.me/api/portraits/men/8.jpg',
+'Software Developer', 
+'Hello my name is Shawn and I am an aspiring software developer', 
+'https://www.linkedin.com/in/shawnfuller/',
+'https://github.com/shawnfuller',
+'http://shawnfuller.com/', 0, "", 6,
+'shawnfuller', 'shawnfuller'
+);
+INSERT INTO skills (front_html, front_css, front_javascript, front_ajax, front_jquery, front_bootstrap, front_react, front_angular, 
+	back_mysql, back_node, back_php, back_express, back_mongodb, back_java, back_python, back_c_sharp, 
+	design_photoshop, design_gimp, design_illustrator, design_inkscape, design_coreldraw, design_adobe_indesign, profileId)
+VALUES (true, true, true, false, true, true, false, false,
+	true, true, false, true, false, true, true, false, 
+	false, false, false, false, false, false, 6);
+
+INSERT INTO projects (name, img_url, description, profileId)
+VALUES ('Mobile phone app', '', 'IOS mobile phone app', 6);
+
+INSERT INTO profiles (name, img_url, title, about, linkedin_url, github_url, personal_url, endorsements, endorsed_people, linkedin_id, username, password)
+VALUES ('Paige Olsen', 
+'https://randomuser.me/api/portraits/women/59.jpg',
+'Fullstack Developer', 
+'Hello my name is Paige and I am an aspiring fullstack developer', 
+'https://www.linkedin.com/in/paigeolsen/',
+'https://github.com/paigeolsen',
+'http://paigeolsen.com/', 0, "", 7,
+'paigeolsen', 'paigeolsen'
+);
+
+INSERT INTO skills (front_html, front_css, front_javascript, front_ajax, front_jquery, front_bootstrap, front_react, front_angular, 
+	back_mysql, back_node, back_php, back_express, back_mongodb, back_java, back_python, back_c_sharp, 
+	design_photoshop, design_gimp, design_illustrator, design_inkscape, design_coreldraw, design_adobe_indesign, profileId)
+VALUES (true, true, true, false, true, true, false, false, 
+	false, false, false, false, false, false, 
+	true, true, true, true, true, true, true, true, 7);
+
+INSERT INTO projects (name, img_url, description, profileId)
+VALUES ('Bamazon', '', 'Bamazon web site', 7);
+
+INSERT INTO profiles (name, img_url, title, about, linkedin_url, github_url, personal_url, endorsements, endorsed_people, linkedin_id, username, password)
+VALUES ('Anton Gerlach', 
+'https://randomuser.me/api/portraits/men/53.jpg',
+'Backend Developer', 
+'Hello my name is Anton and I am an aspiring backend developer', 
+'https://www.linkedin.com/in/antongerlach/',
+'https://github.com/antongerlach',
+'http://antongerlach.com/', 0, "", 8,
+'antongerlach', 'antongerlach'
+);
+
+INSERT INTO skills (front_html, front_css, front_javascript, front_ajax, front_jquery, front_bootstrap, front_react, front_angular, 
+	back_mysql, back_node, back_php, back_express, back_mongodb, back_java, back_python, back_c_sharp, 
+	design_photoshop, design_gimp, design_illustrator, design_inkscape, design_coreldraw, design_adobe_indesign, profileId)
+VALUES (false, false, true, false, false, false, false, false, 
+	false, false, false, false, false, false, 
+	true, true, true, true, true, true, true, true, 8);
+
+INSERT INTO projects (name, img_url, description, profileId)
+VALUES ('Email server', '', 'Mail.com email server', 8);
+
+INSERT INTO profiles (name, img_url, title, about, linkedin_url, github_url, personal_url, endorsements, endorsed_people, linkedin_id, username, password)
+VALUES ('Noah Vargas', 
+'https://randomuser.me/api/portraits/men/9.jpg',
+'Frontend Developer', 
+'Hello my name is Noah and I am an aspiring frontend developer', 
+'https://www.linkedin.com/in/noahvargas/',
+'https://github.com/noahvargas',
+'http://noahvargas.com/', 0, "", 9,
+'noahvargas', 'noahvargas'
+);
+
+INSERT INTO skills (front_html, front_css, front_javascript, front_ajax, front_jquery, front_bootstrap, front_react, front_angular, 
+	back_mysql, back_node, back_php, back_express, back_mongodb, back_java, back_python, back_c_sharp, 
+	design_photoshop, design_gimp, design_illustrator, design_inkscape, design_coreldraw, design_adobe_indesign, profileId)
+VALUES (true, true, true, true, true, true, true, true,
+	false, false, false, false, false, false, false, false,
+	false, false, false, false, false, false, 9);
+
+INSERT INTO projects (name, img_url, description, profileId)
+VALUES ('Hearts', '', 'Hearts card game web site', 9);
+
+INSERT INTO profiles (name, img_url, title, about, linkedin_url, github_url, personal_url, endorsements, endorsed_people, linkedin_id, username, password)
+VALUES ('Ludovicus Klijn', 
+'https://randomuser.me/api/portraits/men/41.jpg',
+'Web Developer', 
+'Hello my name is Laura and I am an aspiring web developer', 
+'https://www.linkedin.com/in/ludovicusklijn/',
+'https://github.com/ludovicusklijn',
+'http://ludovicusklijn.com/', 0, "", 10,
+'ludovicusklijn','ludovicusklijn'
+);
+
+INSERT INTO skills (front_html, front_css, front_javascript, front_ajax, front_jquery, front_bootstrap, front_react, front_angular, 
+	back_mysql, back_node, back_php, back_express, back_mongodb, back_java, back_python, back_c_sharp, 
+	design_photoshop, design_gimp, design_illustrator, design_inkscape, design_coreldraw, design_adobe_indesign, profileId)
+VALUES (true, true, true, true, true, true, true, true,
+	false, false, false, false, false, false, false, false,
+	false, false, false, false, false, false, 10);
+
+INSERT INTO projects (name, img_url, description, profileId)
+VALUES ('Company web site', '', 'Hi-Tech web site', 10);
